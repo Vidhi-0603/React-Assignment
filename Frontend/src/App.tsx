7a86df4dd79356efe2f979772ec82e45b8d778b3
Vidhilika Gupta <gvidhilika0606@gmail.com>
2026-01-28 22:19:14 +0530

    feat: enhance DataTable with multiple selection and pagination functionality

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { DataTable, type DataTablePageEvent } from "primereact/datatable";
+import {
+  DataTable,
+  type DataTablePageEvent,
+  type DataTableSelectionMultipleChangeEvent,
+} from "primereact/datatable";
 import { Column } from "primereact/column";
 import { fetchRows } from "./fetchRows";
 import type { ArtWorks } from "./types";
 
 function App() {
   const [currentPageRows, setCurrentPageRows] = useState<ArtWorks[]>([]); //the data from API
-  const [rowsPerPage, setRowsPerPage] = useState<number>(0); //no of rows in a page
+  const [rowsPerPage, setRowsPerPage] = useState<number>(1); //no of rows in a page
   const [first, setFirst] = useState<number>(0); // paginator position
   const [totalRecords, setTotalRecords] = useState<number>(0);
+  const [selectedArtworksIDs, setselectedArtworksIDs] = useState<Set<number>>(
+    new Set(),
+  );
+
+  const currentSelectedArtworks = currentPageRows.filter((artwork) =>
+    selectedArtworksIDs.has(artwork.id),
+  );
 
   const onPageChangeEvent = async (e: DataTablePageEvent) => {
     setFirst(e.first);
-    const page = Math.floor(e.first / rowsPerPage) + 1;
+    setRowsPerPage(e.rows);
+
+    const page = Math.floor(e.first / e.rows) + 1;
     const result = await fetchRows(page);
     setCurrentPageRows(result.data);
   };
@@ -28,17 +41,38 @@ function App() {
     fetchData(1);
   }, []);
 
+  const onSelectionChangeEvent = (
+    e: DataTableSelectionMultipleChangeEvent<ArtWorks[]>,
+  ) => {
+    const selectedRows: ArtWorks[] = e.value ?? [];
+
+    setselectedArtworksIDs((previousRows) => {
+      const newRows = new Set(previousRows);
+      currentPageRows.forEach((row) => newRows.delete(row.id));
+      selectedRows.forEach((row) => {
+        newRows.add(row.id);
+      });
+      return newRows;
+    });
+  };
+
   return (
     <>
       <DataTable
         value={currentPageRows}
+        dataKey="id"
         paginator
         lazy
         rows={rowsPerPage}
         first={first}
         totalRecords={totalRecords}
         onPage={onPageChangeEvent}
+        selection={currentSelectedArtworks}
+        selectionMode="multiple"
+        selectionPageOnly
+        onSelectionChange={onSelectionChangeEvent}
       >
+        <Column selectionMode="multiple"></Column>
         <Column field="title" header="Title"></Column>
         <Column field="place_of_origin" header="Place of origin"></Column>
         <Column field="artist_display" header="Artist Display"></Column>
