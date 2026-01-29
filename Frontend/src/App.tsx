452fcc0e1f601b1458cf3c12db1855e7ef10fffb
Vidhilika Gupta <gvidhilika0606@gmail.com>
2026-01-29 17:41:48 +0530

    feat: add custom row selection and overlay panel for DataTable

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import {
   DataTable,
@@ -8,6 +8,13 @@ import {
 import { Column } from "primereact/column";
 import { fetchRows } from "./fetchRows";
 import type { ArtWorks } from "./types";
+import { Button } from "primereact/button";
+import { OverlayPanel } from "primereact/overlaypanel";
+import {
+  InputNumber,
+  type InputNumberValueChangeEvent,
+} from "primereact/inputnumber";
+import type { Nullable } from "primereact/ts-helpers";
 
 function App() {
   const [currentPageRows, setCurrentPageRows] = useState<ArtWorks[]>([]); //the data from API
@@ -17,6 +24,9 @@ function App() {
   const [selectedArtworksIDs, setselectedArtworksIDs] = useState<Set<number>>(
     new Set(),
   );
+  const [selectedNoOfRows, setSelectedNoOfRows] =
+    useState<Nullable<number | null>>(0);
+  const op = useRef<OverlayPanel>(null);
 
   const currentSelectedArtworks = currentPageRows.filter((artwork) =>
     selectedArtworksIDs.has(artwork.id),
@@ -56,6 +66,77 @@ function App() {
     });
   };
 
+  const customSelection = (
+    <div>
+      <Button
+        icon="pi pi-chevron-down"
+        text
+        rounded
+        size="small"
+        onClick={(e) => op.current?.toggle(e)}
+      />
+    </div>
+  );
+
+  const onCustomSelection = () => {
+    if (!selectedNoOfRows || selectedNoOfRows < 0) return;
+
+    setselectedArtworksIDs((prevSelectedIDs) => {
+      const newSelectedIDs = new Set(prevSelectedIDs);
+
+      // Deselect rows
+      if (newSelectedIDs.size > selectedNoOfRows) {
+        let rowsToDelete = newSelectedIDs.size - selectedNoOfRows;
+
+        for (const row of currentPageRows) {
+          if (rowsToDelete === 0) break;
+          if (newSelectedIDs.has(row.id)) {
+            newSelectedIDs.delete(row.id);
+            rowsToDelete--;
+          }
+        }
+        return newSelectedIDs;
+      }
+
+      // Select remaining rows
+      let remainingRowsToSelect = selectedNoOfRows - newSelectedIDs.size;
+
+      for (const row of currentPageRows) {
+        if (remainingRowsToSelect === 0) break;
+        if (!newSelectedIDs.has(row.id)) {
+          newSelectedIDs.add(row.id);
+          remainingRowsToSelect--;
+        }
+      }
+
+      return newSelectedIDs;
+    });
+
+    op.current?.hide();
+  };
+
+
+  useEffect(() => {
+    if (!selectedNoOfRows) return;
+
+    setselectedArtworksIDs((prev) => {
+      if (prev.size >= selectedNoOfRows) return prev;
+
+      const newSelectedIDs = new Set(prev);
+      let remainingRowsToSelect = selectedNoOfRows - newSelectedIDs.size;
+
+      for (const row of currentPageRows) {
+        if (remainingRowsToSelect === 0) break;
+        if (!newSelectedIDs.has(row.id)) {
+          newSelectedIDs.add(row.id);
+          remainingRowsToSelect--;
+        }
+      }
+
+      return newSelectedIDs;
+    });
+  }, [currentPageRows, selectedNoOfRows]);
+
   return (
     <>
       <DataTable
@@ -72,7 +153,13 @@ function App() {
         selectionPageOnly
         onSelectionChange={onSelectionChangeEvent}
       >
-        <Column selectionMode="multiple"></Column>
+        <Column
+          selectionMode="multiple"
+          header={customSelection}
+          sortable={false}
+          headerStyle={{ width: "4rem" }}
+        ></Column>
+
         <Column field="title" header="Title"></Column>
         <Column field="place_of_origin" header="Place of origin"></Column>
         <Column field="artist_display" header="Artist Display"></Column>
@@ -80,6 +167,22 @@ function App() {
         <Column field="date_start" header="Start Date"></Column>
         <Column field="date_end" header="End Date"></Column>
       </DataTable>
+      <OverlayPanel ref={op}>
+        <InputNumber
+          value={selectedNoOfRows}
+          onValueChange={(e: InputNumberValueChangeEvent) =>
+            setSelectedNoOfRows(e.value)
+          }
+          placeholder="Select N rows"
+        />
+        <Button
+          label="Apply"
+          text
+          rounded
+          size="small"
+          onClick={onCustomSelection}
+        ></Button>
+      </OverlayPanel>
     </>
   );
 }
